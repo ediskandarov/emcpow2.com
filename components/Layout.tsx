2f3a32bb8d1a5bf42fa4152b524b26526c959723
Eduard Iskandarov <eduard.iskandarov@yandex.ru>
2021-02-02 22:49:05 +0500

    💄 apply styles

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,13 +14,6 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%22100%22>🛠</text></svg>" />
     </Head>
-    {/* <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-      </nav>
-    </header> */}
     {children}
   </div>
 )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,30 @@
 import Layout from '../components/Layout'
+import Image from 'next/image'
 import Link from 'next/link'
+
 
 
 const IndexPage = (): JSX.Element => (
   <Layout title="Eduard Iskandarov's home page">
     <div className="h-2 bg-indigo-600" />
-
-    <div className="text-center break-words text-gray-900 dark:text-white">
-      <h1>Hi! I'm Eduard Iskandarov (<Link href="https://twitter.com/emcpow2">@emcpow2</Link>) 👋.</h1>
-      <p>I'm a software engineer and entrepreneur from Russia. 🇷🇺</p>
-      <p>I'm currently exploring ideas to build a tech startup.</p>
-      <p>I have been dealing with high load and big data challenges in the biggest Russian IT Company and helped a Southeast Asian fintech startup cross the Death Valley.</p>
-      <p>Aside from software, I built teams that work autonomously and yield outstanding results.</p>
-      <p>I like to code serverless applications using Python, TypeScript, focus on security and automation.</p>
+    <div className="container mx-auto sm:max-w-screen-md">
+      <figure className="sm:w-max mx-auto sm:mt-6">
+        <Image
+          className="sm:rounded-3xl"
+          src="/me.jpg"
+          alt="Picture of the Eduard"
+          width={500}
+          height={500}
+        />
+      </figure>
+      <main className="text-center break-words text-gray-900 dark:text-white text-lg">
+        <h1 className="mt-6 text-3xl">Hi! I'm Eduard Iskandarov (<Link href="https://twitter.com/emcpow2">@emcpow2</Link>) 👋.</h1>
+        <p className="mt-6">I'm a software engineer and entrepreneur from Russia. 🇷🇺</p>
+        <p className="mt-6">I'm currently exploring ideas to build a tech startup.</p>
+        <p className="mt-6">I have been dealing with high load and big data challenges in the biggest Russian IT Company and helped a Southeast Asian fintech startup cross the Death Valley.</p>
+        <p className="mt-6">Aside from software, I built teams that work autonomously and yield outstanding results.</p>
+        <p className="my-6">I like to code serverless applications using Python, TypeScript, focus on security and automation.</p>
+      </main>
     </div>
   </Layout>
 )
