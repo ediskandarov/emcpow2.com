1e613ee79396400fac39bab307227d52bb0607de
Eduard Iskandarov <eduard.iskandarov@yandex.ru>
2021-02-02 11:36:50 +0500

    💬 add index page contents

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,16 @@ import Link from 'next/link'
 
 const IndexPage = (): JSX.Element => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1 className="text-red-500">Hello Next.js 👋</h1>
-    <p>Hi! I'm Eduard Iskandarov (<Link href="https://twitter.com/emcpow2">@emcpow2</Link>) 👋.</p>
-    <p>I'm a software engineer and entrepreneur from Russia. 🇷🇺</p>
-    <p>I'm currently exploring ideas to build a tech startup.</p>
-    <p>I have been dealing with high load and big data challenges in the biggest Russian IT Company and helped a Southeast Asian fintech startup cross the Death Valley.</p>
-    <p>Aside from software, I built teams that work autonomously and yield outstanding results.</p>
-    <p>I like to code serverless applications using Python, TypeScript, focus on security and automation.</p>
+    <div className="h-2 bg-indigo-600" />
+
+    <div className="text-center break-words">
+      <h1>Hi! I'm Eduard Iskandarov (<Link href="https://twitter.com/emcpow2">@emcpow2</Link>) 👋.</h1>
+      <p>I'm a software engineer and entrepreneur from Russia. 🇷🇺</p>
+      <p>I'm currently exploring ideas to build a tech startup.</p>
+      <p>I have been dealing with high load and big data challenges in the biggest Russian IT Company and helped a Southeast Asian fintech startup cross the Death Valley.</p>
+      <p>Aside from software, I built teams that work autonomously and yield outstanding results.</p>
+      <p>I like to code serverless applications using Python, TypeScript, focus on security and automation.</p>
+    </div>
   </Layout>
 )
 
