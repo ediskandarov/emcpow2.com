5bf9862d526ebc99ca1568ccd86eefb1c373872d
Eduard Iskandarov <eduard.iskandarov@yandex.ru>
2021-02-02 23:21:03 +0500

    🎨 add some fancyness

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
     return (
       <Html className="dark">
         <Head />
-        <body className="bg-white dark:bg-gray-800">
+        <body className="bg-white dark:bg-black">
           <Main />
           <NextScript />
         </body>
