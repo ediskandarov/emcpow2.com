008e3172dba87956be605b8f85b1094403b3a3ac
Eduard Iskandarov <eduard.iskandarov@yandex.ru>
2021-02-08 15:41:42 +0500

    🎨 move container to layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -17,7 +17,8 @@ const Layout = ({ children, title = "This is the default title" }: Props) => (
         href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%22100%22>🛠</text></svg>"
       />
     </Head>
-    {children}
+    <div className="h-2 bg-yellow-600" />
+    <div className="container mx-auto sm:max-w-screen-md">{children}</div>
   </div>
 );
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,47 +5,44 @@ import Link from "next/link";
 
 const IndexPage = (): JSX.Element => (
   <Layout title="Eduard Iskandarov's home page">
-    <div className="h-2 bg-yellow-600" />
-    <div className="container mx-auto sm:max-w-screen-md">
-      <figure className="sm:w-max mx-auto sm:mt-6">
-        <Image
-          className="sm:rounded-3xl"
-          src="/me.jpg"
-          alt="Picture of Eduard"
-          width={500}
-          height={500}
-        />
-      </figure>
-      <main className="text-center break-words text-gray-900 dark:text-white text-lg">
-        <h1 className="mt-6 text-3xl">
-          Hi! I'm Eduard Iskandarov (
-          <Link prefetch={false} href="https://twitter.com/emcpow2">
-            <a className="text-yellow-600">@emcpow2</a>
-          </Link>
-          ) 👋.
-        </h1>
-        <p className="mt-6">
-          I'm a software engineer and entrepreneur from Russia. 🇷🇺
-        </p>
-        <p className="mt-6">
-          I'm currently exploring ideas to build a tech startup. 🎉
-        </p>
-        <p className="mt-6">
-          I have been dealing with high load and big data challenges in the
-          biggest Russian IT Company and helped a Southeast Asian fintech
-          startup cross the Death Valley. 👨‍🔧
-        </p>
-        <p className="mt-6">
-          Aside from software, I built teams that work autonomously and yield
-          outstanding results. 🍰
-        </p>
-        <p className="mt-6">
-          I like to code serverless applications using Python, TypeScript, focus
-          on security and automation. 🚀
-        </p>
-      </main>
-      <SocialLinks />
-    </div>
+    <figure className="sm:w-max mx-auto sm:mt-6">
+      <Image
+        className="sm:rounded-3xl"
+        src="/me.jpg"
+        alt="Picture of Eduard"
+        width={500}
+        height={500}
+      />
+    </figure>
+    <main className="text-center break-words text-gray-900 dark:text-white text-lg">
+      <h1 className="mt-6 text-3xl">
+        Hi! I'm Eduard Iskandarov (
+        <Link prefetch={false} href="https://twitter.com/emcpow2">
+          <a className="text-yellow-600">@emcpow2</a>
+        </Link>
+        ) 👋.
+      </h1>
+      <p className="mt-6">
+        I'm a software engineer and entrepreneur from Russia. 🇷🇺
+      </p>
+      <p className="mt-6">
+        I'm currently exploring ideas to build a tech startup. 🎉
+      </p>
+      <p className="mt-6">
+        I have been dealing with high load and big data challenges in the
+        biggest Russian IT Company and helped a Southeast Asian fintech startup
+        cross the Death Valley. 👨‍🔧
+      </p>
+      <p className="mt-6">
+        Aside from software, I built teams that work autonomously and yield
+        outstanding results. 🍰
+      </p>
+      <p className="mt-6">
+        I like to code serverless applications using Python, TypeScript, focus
+        on security and automation. 🚀
+      </p>
+    </main>
+    <SocialLinks />
   </Layout>
 );
 
